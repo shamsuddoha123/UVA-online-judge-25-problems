d45428deadcbe653d32b347820ed8254276d5344
Md. Shamsuddoha <154912721+shamsuddoha123@users.noreply.github.com>
2024-01-20 01:40:38 +0600

    Update UVA 10783 - Odd Sum.cpp

--- UVA 10783 - Odd Sum.cpp
+++ UVA 10783 - Odd Sum.cpp
@@ -1,14 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int T,a,b,i,j,sum,smallest;
+    int T,a,b,i,j,sum;
     scanf("%d", &T);
     for(i=1;i<=T;i++)
     {
         scanf("%d\n%d", &a, &b);
-        smallest=(a<b)?a:b;
         sum=0;
-        for(j=smallest;j<=b;j++)
+        for(j=a;j<=b;j++)
         {
             if(j%2!=0)
             {
